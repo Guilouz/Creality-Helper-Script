c1a5ad0f3ec2814edaa11d01b268d56216496e9f
Aleksandr Liashov <aliashov@fnddev.cc>
2025-05-29 14:09:24 +0300

    refactor: Remove specific items from E5M install/remove menus
    Removes the following items from the E5M-specific installation and removal menus:
    - Klipper Adaptive Meshing & Purging
    - Nozzle Cleaning Fan Control
    - M600 Support
    - Git Backup

--- scripts/menu/E5M/install_menu_E5M.sh
+++ scripts/menu/E5M/install_menu_E5M.sh
@@ -17,29 +17,25 @@ function install_menu_ui_e5m() {
   menu_option ' 5' 'Install' 'Klipper Gcode Shell Command'
   hr
   subtitle '•IMPROVEMENTS:'
-  menu_option ' 6' 'Install' 'Klipper Adaptive Meshing & Purging'
-  menu_option ' 7' 'Install' 'Buzzer Support'
-  menu_option ' 8' 'Install' 'Nozzle Cleaning Fan Control'
-  menu_option ' 9' 'Install' 'Fans Control Macros'
-  menu_option '10' 'Install' 'Improved Shapers Calibrations'
-  menu_option '11' 'Install' 'Useful Macros'
-  menu_option '12' 'Install' 'Save Z-Offset Macros'
-  menu_option '13' 'Install' 'Screws Tilt Adjust Support'
-  menu_option '14' 'Install' 'M600 Support'
-  menu_option '15' 'Install' 'Git Backup'
+  menu_option ' 6' 'Install' 'Buzzer Support'
+  menu_option ' 7' 'Install' 'Fans Control Macros'
+  menu_option ' 8' 'Install' 'Improved Shapers Calibrations'
+  menu_option ' 9' 'Install' 'Useful Macros'
+  menu_option '10' 'Install' 'Save Z-Offset Macros'
+  menu_option '11' 'Install' 'Screws Tilt Adjust Support'
   hr
   subtitle '•CAMERA:'
-  menu_option '16' 'Install' 'Moonraker Timelapse'
-  menu_option '17' 'Install' 'Camera Settings Control'
-  menu_option '18' 'Install' 'USB Camera Support'
+  menu_option '12' 'Install' 'Moonraker Timelapse'
+  menu_option '13' 'Install' 'Camera Settings Control'
+  menu_option '14' 'Install' 'USB Camera Support'
   hr
   subtitle '•REMOTE ACCESS:'
-  menu_option '19' 'Install' 'OctoEverywhere'
-  menu_option '20' 'Install' 'Moonraker Obico'
-  menu_option '21' 'Install' 'GuppyFLO'
-  menu_option '22' 'Install' 'Mobileraker Companion'
-  menu_option '23' 'Install' 'OctoApp Companion'
-  menu_option '24' 'Install' 'SimplyPrint'
+  menu_option '15' 'Install' 'OctoEverywhere'
+  menu_option '16' 'Install' 'Moonraker Obico'
+  menu_option '17' 'Install' 'GuppyFLO'
+  menu_option '18' 'Install' 'Mobileraker Companion'
+  menu_option '19' 'Install' 'OctoApp Companion'
+  menu_option '20' 'Install' 'SimplyPrint'
   hr
   inner_line
   hr
@@ -92,12 +88,6 @@ function install_menu_e5m() {
           run "install_gcode_shell_command" "install_menu_ui_e5m"
         fi;;
       6)
-        if [ -d "$KAMP_FOLDER" ]; then
-          error_msg "Klipper Adaptive Meshing & Purging is already installed!"
-        else
-          run "install_kamp" "install_menu_ui_e5m"
-        fi;;
-      7)
         if [ -f "$BUZZER_FILE" ]; then
           error_msg "Buzzer Support is already installed!"
         elif [ ! -f "$KLIPPER_SHELL_FILE" ]; then
@@ -105,19 +95,13 @@ function install_menu_e5m() {
         else
           run "install_buzzer_support" "install_menu_ui_e5m"
         fi;;
-      8)
-        if [ -d "$NOZZLE_CLEANING_FOLDER" ]; then
-          error_msg "Nozzle Cleaning Fan Control is already installed!"
-        else
-          run "install_nozzle_cleaning_fan_control" "install_menu_ui_e5m"
-        fi;;
-      9)
+      7)
         if [ -f "$HS_CONFIG_FOLDER/e5m_custom_fan_definitions_applied.flag" ]; then
           error_msg "E5M Custom Fan Macros are already applied!"
         else
           run "install_fans_control_macros" "install_menu_ui_e5m"
         fi;;
-      10)
+      8)
         if [ -d "$IMP_SHAPERS_FOLDER" ]; then
           error_msg "Improved Shapers Calibrations are already installed!"
         elif [ -d "$GUPPY_SCREEN_FOLDER" ]; then
@@ -127,7 +111,7 @@ function install_menu_e5m() {
         else
           run "install_improved_shapers" "install_menu_ui_e5m"
         fi;;
-      11)
+      9)
         if [ -f "$USEFUL_MACROS_FILE" ]; then
           error_msg "Useful Macros are already installed!"
         elif [ ! -f "$KLIPPER_SHELL_FILE" ]; then
@@ -135,35 +119,19 @@ function install_menu_e5m() {
         else
           run "install_useful_macros" "install_menu_ui_e5m"
         fi;;
-      12)
+      10)
         if [ -f "$SAVE_ZOFFSET_FILE" ]; then
           error_msg "Save Z-Offset Macros are already installed!"
         else
           run "install_save_zoffset_macros" "install_menu_ui_e5m"
         fi;;
-      13)
+      11)
         if [ -f "$SCREWS_ADJUST_FILE" ]; then
           error_msg "Screws Tilt Adjust Support is already installed!"
         else
           run "install_screws_tilt_adjust" "install_menu_ui_e5m"
         fi;;
-      14)
-        if [ -f "$M600_SUPPORT_FILE" ]; then
-          error_msg "M600 Support is already installed!"
-        else
-          run "install_m600_support" "install_menu_ui_e5m"
-        fi;;
-      15)
-        if [ -f "$GIT_BACKUP_FILE" ]; then
-          error_msg "Git Backup is already installed!"
-        elif [ ! -f "$ENTWARE_FILE" ]; then
-          error_msg "Entware is needed, please install it first!"
-        elif [ ! -f "$KLIPPER_SHELL_FILE" ]; then
-          error_msg "Klipper Gcode Shell Command is needed, please install it first!"
-        else
-          run "install_git_backup" "install_menu_ui_e5m"
-        fi;;
-      16)
+      12)
         if [ -f "$TIMELAPSE_FILE" ]; then
           error_msg "Moonraker Timelapse is already installed!"
         elif [ ! -f "$ENTWARE_FILE" ]; then
@@ -171,7 +139,7 @@ function install_menu_e5m() {
         else
           run "install_moonraker_timelapse" "install_menu_ui_e5m"
         fi;;
-      17)
+      13)
         if [ -f "$CAMERA_SETTINGS_FILE" ]; then
           error_msg "Camera Settings Control is already installed!"
         elif v4l2-ctl --list-devices | grep -q 'CCX2F3299' && [ ! -f "$INITD_FOLDER"/S50usb_camera ]; then
@@ -181,7 +149,7 @@ function install_menu_e5m() {
         else
           run "install_camera_settings_control" "install_menu_ui_e5m"
         fi;;
-      18)
+      14)
         if [ -f "$USB_CAMERA_FILE" ]; then
           error_msg "Camera USB Support is already installed!"
         elif [ ! -f "$ENTWARE_FILE" ]; then
@@ -189,7 +157,7 @@ function install_menu_e5m() {
         else
           run "install_usb_camera" "install_menu_ui_e5m"
         fi;;
-      19)
+      15)
         if [ ! -d "$MOONRAKER_FOLDER" ]; then
           error_msg "Moonraker and Nginx are needed, please install them first!"
         elif [ ! -d "$FLUIDD_FOLDER" ] && [ ! -d "$MAINSAIL_FOLDER" ]; then
@@ -199,7 +167,7 @@ function install_menu_e5m() {
         else
           run "install_octoeverywhere" "install_menu_ui_e5m"
         fi;;
-      20)
+      16)
         if [ ! -d "$MOONRAKER_FOLDER" ]; then
           error_msg "Moonraker and Nginx are needed, please install them first!"
         elif [ ! -d "$FLUIDD_FOLDER" ] && [ ! -d "$MAINSAIL_FOLDER" ]; then
@@ -209,13 +177,13 @@ function install_menu_e5m() {
         else
           run "install_moonraker_obico" "install_menu_ui_e5m"
         fi;;
-      21)
+      17)
         if [ ! -d "$MOONRAKER_FOLDER" ] && [ ! -d "$NGINX_FOLDER" ]; then
           error_msg "Moonraker and Nginx are needed, please install them first!"
         else
           run "install_guppyflo" "install_menu_ui_e5m"
         fi;;
-      22)
+      18)
         if [ -d "$MOBILERAKER_COMPANION_FOLDER" ]; then
           error_msg "Mobileraker Companion is already installed!"
         elif [ ! -d "$MOONRAKER_FOLDER" ]; then
@@ -227,7 +195,7 @@ function install_menu_e5m() {
         else
           run "install_mobileraker_companion" "install_menu_ui_e5m"
         fi;;
-      23)
+      19)
         if [ -d "$OCTOAPP_COMPANION_FOLDER" ]; then
           error_msg "OctoApp Companion is already installed!"
         elif [ ! -d "$MOONRAKER_FOLDER" ]; then
@@ -239,7 +207,7 @@ function install_menu_e5m() {
         else
           run "install_octoapp_companion" "install_menu_ui_e5m"
         fi;;
-      24)
+      20)
         if grep -q "\[simplyprint\]" "$MOONRAKER_CFG"; then
           error_msg "SimplyPrint is already installed!"
         elif [ ! -d "$MOONRAKER_FOLDER" ]; then
